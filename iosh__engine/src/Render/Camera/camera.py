da2eb00c0a0914c412154a64bacbf45ea3a9077f
IOSHED <104500113+IOSHED@users.noreply.github.com>
2022-09-26 20:23:48 +0300

    Add files via upload

---
+++ iosh__engine/src/Render/Camera/camera.py
@@ -0,0 +1,44 @@
+import math
+import numpy as np
+from src.Render.Matrix_functions.camera_matrix import CameraMatrix
+from src.Render.Camera.control_camera import ControlCamera
+from src.Render.Matrix_functions.matrix import MatrixFunctions
+
+
+class Camera:
+    def __init__(self, render, position):
+        self.render = render
+        self.control = ControlCamera(self)
+
+        self.position = np.array([*position, 1.0])
+
+        self.forward = np.array([0, 0, 1, 1])
+        self.up = np.array([0, 1, 0, 1])
+        self.right = np.array([1, 0, 0, 1])
+        self.left = np.array([0, 1, 0, 1])
+
+        self.fov_height = math.pi / 3
+        self.fov_width = self.fov_height * (render.window.height / render.window.width)
+
+        self.near_plane = 1
+        self.far_plane = 100
+
+    def camera_matrix(self):
+        return CameraMatrix.translate_matrix(self) @ CameraMatrix.rotate_matrix(self)
+
+    def camera_yaw(self, angle):
+        rotate = MatrixFunctions.rotate_y(angle)
+        self.forward = self.forward @ rotate
+        self.left = self.left @ rotate
+        self.right = self.right @ rotate
+        self.up = self.up @ rotate
+
+    def camera_pitch(self, angle):
+        rotate = MatrixFunctions.rotate_x(angle)
+        self.forward = self.forward @ rotate
+        self.left = self.left @ rotate
+        self.right = self.right @ rotate
+        self.up = self.up @ rotate
+
+    def control_camera(self):
+        self.control.control_camera()
